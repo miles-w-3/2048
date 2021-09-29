18bd588df6f25f8be84d41aacd3dd795e0f8aaf7
root <root@DESKTOP-TGTOVDD.localdomain>
2021-09-28 22:07:28 -0400

    Working keyListener, up shift in progress

--- src/App.js
+++ src/App.js
@@ -15,36 +15,37 @@ class Tile extends React.Component {
 }
 
 class Board extends React.Component {
-
+    // initialize tiles array within state
     constructor(props) {
         super(props);
         this.state = {
             // keep track of tile values in each location
             tiles: Array(16).fill(0)
         }
-
-        // fill two starter tiles
-        this.fillNewTile();
-        this.fillNewTile();
     }
 
-    // componentDidMount, put keylistener in there
-    useEffect(() => {
-        document.addEventListener('keypress', (e) => {
-            if (e.code == "ArrowUp") {
-                this.shiftUp();
-            }
-        })
-    }); 
+    componentDidMount() {
+        // set key listener for arrowkeys
+        document.addEventListener('keyup', e => this.onKeyPress(e), true);
 
-    // spawn a new tile on an empty space in the board
-    fillNewTile() {
+        // fill two starter tiles
+        const newTiles = this.state.tiles.slice();
+        this.fillNewTile(newTiles);
+        this.fillNewTile(newTiles);
+        console.count("New tiles array is " + newTiles)
+        this.setState({tiles: newTiles});
+    }
+
+    // spawn a new tile on an empty space in the given tile array
+    fillNewTile(tiles_array) {
+        // TODO: Could trigger game loss if length of open is zero, meaning all spaces are currently filled
         let open = []; // track all open slots, one will be filled with a new tile
-        for (let i = 0; i < this.state.tiles.length; i++) {
-            if (this.state.tiles[i] === 0) {
-                open.push(i)
+        for (let i = 0; i < tiles_array.length; i++) {
+            if (tiles_array[i] === 0) {
+                open.push(i);
             }
         }
+        console.count("Open array is " + open)
         // choose open index to make new tile in
         let openIndex = Math.floor(Math.random() * open.length);
         // get the corresponding index on the full board
@@ -55,10 +56,9 @@ class Board extends React.Component {
         let chance = Math.floor(Math.random() * 9)
 
         chance === 0 ? value = 4 : value = 2;
-
-        // now set the new tile on the board TODO: should be moved away from direct assignment using setState
-        this.state.tiles[boardIndex] = value;
+        tiles_array[boardIndex] = value;
     }
+
 
     // render the board on screen
     render() {
@@ -93,52 +93,62 @@ class Board extends React.Component {
 
     shiftUp() {
         // TODO: need a list of indexes that have already merged this cycle so that double merges don't happen as you go up
-        let merged = Set()
-        // continually shift tiles up to the top if there is an empty space within the board 
-        for (let i = 15; i >=0; i--) {
+        let tiles_new = this.state.tiles.slice();
+        //let merged = Set()
+        // continually shift tiles up to the top if there is an empty space within the board
+        for (let i = 15; i >= 0; i--) {
             // if there is a tile at the current place and there is a valid space above
-            if (this.state.tiles[i] && i-4 >= 0) {
+            if (tiles_new[i] && i - 4 >= 0) {
                 // move into empty space
-                if (this.state.tiles[i-4] === 0) {
+                if (tiles_new[i - 4] === 0) {
                     // swap tile locations
-                    this.state.tiles[i-4] = this.state.tiles[i] 
-                    this.state.tiles[i] = 0; 
+                    tiles_new[i - 4] = tiles_new[i]
+                    tiles_new[i] = 0;
                 }
                 // otherwise, if the value of the tiles is equal, merge them TODO: Only merge if they have not been merged this time
-                else if (this.state.tiles[i] === this.state.tiles[i-4]) {
+                else if (tiles_new[i] === tiles_new[i - 4]) {
                     // merge tile values 
-                    this.state.tiles[i-4] += this.state.tiles[i] 
-                    this.state.tiles[i] = 0; 
+                    tiles_new[i - 4] += tiles_new[i]
+                    tiles_new[i] = 0; // old location set to zero
                 }
+                // TODO: Experimental: updating state after each move to see if it animates (prolly wont)
+                console.count("tiles is now: " + tiles_new)
+                //this.setState({tiles: tiles_new});
             }
         }
+        // TODO: Only spawn a new tile if any movement occurred. This function could return a true if any tiles moved, and then the main caller, maybe the keyListener, would call this if anything moved
         // spawn a new tile now that the shift is done
-        this.fillNewTile(); // TODO: does this get moved into the event listener?
+        this.fillNewTile(tiles_new); // TODO: does this get moved into the event listener?
+        this.setState({tiles: tiles_new});
+        console.log()
     }
 
-    /*
     // listen for arrow keys
-    onKeyPress(key) {
+    onKeyPress(e) {
+        console.count("In keyPress function")
         // left arrow
-        if (e.keyCode() === 37) {
+        if (e.code === "ArrowLeft") {
 
         }
         // up arrow
-        else if (e.keyCode() === 38) {
-
+        else if (e.code === "ArrowUp") {
+            console.count("In ArrowUp")
+            this.shiftUp();
         }
         // right arrow
-        else if (e.keyCode() === 39) {
+        else if (e.code === 39) {
 
         }
         // down arrow
-        else if (e.keyCode() === 40) {
+        else if (e.code === 40) {
 
-      }*/
+        }
+    }
 }
 
 
 function Game() {
+
     return (
         <div className="game">
             <div className="game-board">
