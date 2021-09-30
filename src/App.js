a09ed7e96654071bf2e3bb0895b4a4f2811ffa49
miles-w-3 <mileswilson2019@gmail.com>
2021-09-30 11:42:24 -0400

    Fully functional up and down

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function Tile(props) {
     // return a square tile
     return (
         <div className="square" style={{backgroundColor: props.color}}>
-            <span>{props.value}</span>
+            <span>{props.value === 0 ? "-" : props.value}</span>
         </div>
     )
 
@@ -26,10 +26,8 @@ class Board extends React.Component {
     }
 
     componentDidMount() {
-        // set key listener for arrowkeys
+        // set key listener for arrow keys
         document.addEventListener('keyup', e => this.onKeyPress(e), true);
-
-
 
         // fill two starter tiles
         const newTiles = this.state.tiles.slice();
@@ -117,14 +115,14 @@ class Board extends React.Component {
         }
     }
 
-    // execute the shift in the tiles list if it is valid, return true if movement occurred
-    shiftCheck(tiles_list, merged, idx, shiftIdx) {
-        // track the index being assessed for shift
+    // move a tile from the idx to the shiftIdx if valid shift is possible
+    executeShift(tiles_list, merged, idx, shiftIdx) {
+        // if there is a tile at the index to move, and the shift index is valid that could move here that could move here
         if (tiles_list[idx] && shiftIdx >= 0 && shiftIdx <= 15) {
-            // move into empty space
+            // if moving to an empty tile
             if (tiles_list[shiftIdx] === 0) {
                 // move tile
-                tiles_list[shiftIdx] = tiles_list[idx]
+                tiles_list[shiftIdx] = tiles_list[idx];
                 tiles_list[idx] = 0;
                 // if this tile has been merged previously, make sure to keep track at the new index
                 if (merged.has(idx)) {
@@ -134,13 +132,32 @@ class Board extends React.Component {
                 return true;
             }
             // otherwise, if the value of the tiles is equal, make sure they haven't been merged during this shift
-            else if (tiles_list[idx] === tiles_list[shiftIdx] && !merged.has(idx) && !merged.has(idx)) {
+            else if (tiles_list[idx] === tiles_list[shiftIdx] && !merged.has(idx) && !merged.has(shiftIdx)) {
                 // merge tile values
                 tiles_list[shiftIdx] += tiles_list[idx]
                 tiles_list[idx] = 0; // old location set to zero
                 // track that the tile has been merged during this shift
                 merged.add(shiftIdx);
                 return true;
+            }
+            // if there is a tile above and it is not able to merge, we need to see if it can move so that this tile
+            // doesn't get blocked
+            else {
+                // calculate the shift interval
+                let shift_interval = shiftIdx - idx;
+                let block_moved = this.executeShift(tiles_list, merged, shiftIdx, shiftIdx + shift_interval);
+                // if the above block was able to move, then we can execute this move
+                if (block_moved) {
+                    // move tile
+                    tiles_list[shiftIdx] = tiles_list[idx];
+                    tiles_list[idx] = 0;
+                    // if this tile has been merged previously, make sure to keep track at the new index
+                    if (merged.has(idx)) {
+                        merged.delete(idx);
+                        merged.add(shiftIdx);
+                    }
+                    return true;
+                }
             }
         }
         return false;
@@ -151,22 +168,31 @@ class Board extends React.Component {
      */
     upShift = (newTiles, merged) => {
         let movementOccurred = false;
+        // loop up until the last row since nothing can be pulled up into the last row
         for (let i = 15; i >= 0; i--) {
-            // shift when possible, track if movement occurred, shift_idx is +/- 4
-            movementOccurred = this.shiftCheck(newTiles, merged, i, i - 4) || movementOccurred;
+            movementOccurred = this.executeShift(newTiles, merged, i, i - 4) || movementOccurred;
         }
-
         return movementOccurred;
     }
 
     downShift = (newTiles, merged) => {
         let movementOccurred = false;
+        // loop up until the first row, nothing will be pulled from above the first row
         for (let i = 0; i <= 15; i++) {
-            console.count("Downshift i is " + i)
-            // shift when possible, track if movement occurred, shift_idx is +/- 4
-            movementOccurred = this.shiftCheck(newTiles, merged, i, i + 4) || movementOccurred;
+            movementOccurred = this.executeShift(newTiles, merged, i, i + 4) || movementOccurred;
         }
-        console.count("Downshift movement occurred is " + movementOccurred)
+        return movementOccurred;
+    }
+
+    leftShift = (newTiles, merged) => {
+        let transformTiles = [];
+        let movementOccurred = false;
+        // loop up until the first row, nothing will be pulled from above the first row
+        for (let j = 0; j < 4; j++) {
+            for (let i = 15; i >= 4; i--) {
+                movementOccurred = this.executeShift(newTiles, merged, i, i - 4) || movementOccurred;
+            }
+        }
         return movementOccurred;
     }
 
